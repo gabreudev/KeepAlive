f4481257206e7f146a9df643a21be9d9a1b6dd73
gabreudev <biel0alves0@gmail.com>
2024-09-22 11:45:58 -0300

    first commit

---
+++ src/main/java/com/gabreudev/KeepAlive/KeepAliveApplication.java
@@ -0,0 +1,13 @@
+package com.gabreudev.KeepAlive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepAliveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepAliveApplication.class, args);
+	}
+
+}
